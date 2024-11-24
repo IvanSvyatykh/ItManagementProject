4c96b3b773d893ed81218be2f3a53c56840989d3
Insignificance <happywin855@gmail.com>
2024-11-24 18:48:19 +0500

    1. Обновлено сообщение при существовании номера 2. Добавлена инлайн клавиатура с кнопкой начать, если номер пользователя существует в базе 3. Добавлены сообщения для Бронирования и Статистики 4. Добавлены кнопки Бронь и Статистика 5. Измена логика отправки сообщений. Теперь бот не отправляет каждый раз сообщение с кухней, а изменяет его 6. Добавлена логика сохранения message_id

--- src/handlers/kitchen_handler.py
+++ src/handlers/kitchen_handler.py
@@ -6,6 +6,8 @@ from config import BOT_TOKEN
 from handlers.supports.answer import (
     KITCHEN_PHOTO_CAPTURE,
     NOT_ALLOWED_FUNC,
+    STATISTIC_MESS,
+    BOOKING_MESS,
 )
 from services.kitchen_service import get_people_on_kitchen
 from handlers.supports.keybords import get_kitchen_keyboard
@@ -13,30 +15,43 @@ from handlers.supports.keybords import get_kitchen_keyboard
 router = Router()
 
 
-async def send_kitchen_info(chat_id: str) -> None:
+async def send_kitchen_info(chat_id: str, state: FSMContext) -> None:
     bot = Bot(token=BOT_TOKEN)
     kitchen_info = await get_people_on_kitchen()
-    await bot.send_photo(
+    message = await bot.send_photo(
         chat_id=chat_id,
         photo=FSInputFile(kitchen_info[1]),
         caption=KITCHEN_PHOTO_CAPTURE.format(kitchen_info[0]),
         reply_markup=await get_kitchen_keyboard(),
     )
+    await state.update_data(message_id=message.message_id)
 
 
 @router.callback_query(lambda c: c.data == "update_kitchen_info")
-async def update_kitchen_info(callback_query: types.CallbackQuery):
-    await send_kitchen_info(callback_query.message.chat.id)
-
-
-@router.message(Command("kitchen"))
-async def get_kitchen_info(message: types.Message, state: FSMContext):
+async def update_kitchen_info(callback_query: types.CallbackQuery, state: FSMContext):
     user_data = await state.get_data()
+    message_id = user_data.get("message_id")
 
+    kitchen_info = await get_people_on_kitchen()
+    await callback_query.message.bot.edit_message_media(
+        media=types.InputMediaPhoto(
+            media=FSInputFile(kitchen_info[1]),
+            caption=KITCHEN_PHOTO_CAPTURE.format(kitchen_info[0]),
+        ),
+        chat_id=callback_query.message.chat.id,
+        message_id=message_id,
+        reply_markup=await get_kitchen_keyboard(),
+    )
+
+
+@router.callback_query(lambda c: c.data == "kitchen")
+async def get_kitchen_info(callback_query: types.CallbackQuery, state: FSMContext):
+    user_data = await state.get_data()
+    # Ну /kitchen и /kitchen_stat не существует... нужна ли тут вообще какая-то обработка
     if (
-        "phone_number" not in user_data
-        or user_data["phone_number"] is None
+            "phone_number" not in user_data
+            or user_data["phone_number"] is None
     ):
-        await message.answer(text=NOT_ALLOWED_FUNC)
+        await callback_query.message.answer(text=NOT_ALLOWED_FUNC)
     else:
-        await send_kitchen_info(message.chat.id)
+        await send_kitchen_info(callback_query.message.chat.id, state)
--- src/handlers/supports/keybords.py
+++ src/handlers/supports/keybords.py
@@ -24,11 +24,30 @@ async def get_phone_number_keyboard() -> ReplyKeyboardMarkup:
     return markup
 
 
+async def get_start_keyboard() -> ReplyKeyboardMarkup:
+    keyboard_builder = InlineKeyboardBuilder()
+    keyboard_builder.button(
+        text="Начать", callback_data="kitchen"
+    )
+    return keyboard_builder.adjust(1).as_markup(
+        one_time_keyboard=True, resize_keyboard=True
+    )
+
+
 async def get_kitchen_keyboard() -> InlineKeyboardMarkup:
     keyboard_builder = InlineKeyboardBuilder()
     keyboard_builder.button(
         text="Обновить", callback_data="update_kitchen_info"
     )
+    keyboard_builder.button(
+        text="Бронь", callback_data="booking"
+    )
+    keyboard_builder.button(
+        text="Статистика", callback_data="kitchen_statistics"
+    )
     return keyboard_builder.adjust(1).as_markup(
         one_time_keyboard=True, resize_keyboard=True
     )
+
+
+

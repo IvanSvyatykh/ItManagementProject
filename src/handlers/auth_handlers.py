bb1f1f8ca682357ef875803e6d987701c82f01ef
Ivan Svyatykh <zlativansvyat@mail.ru>
2024-11-29 20:48:57 +0500

    прошелся black-ом

--- src/handlers/auth_handlers.py
+++ src/handlers/auth_handlers.py
@@ -7,7 +7,10 @@ from handlers.supports.answer import (
     PHONE_NUMBER_NOT_EXISTS,
 )
 from handlers.supports.state_machine import UserState
-from handlers.supports.keybords import get_phone_number_keyboard, get_start_keyboard
+from handlers.supports.keybords import (
+    get_phone_number_keyboard,
+    get_start_keyboard,
+)
 from services.auth_service import check_phone_number
 
 router = Router()
@@ -26,7 +29,10 @@ async def get_user_password(message: types.Contact, state: FSMContext):
     phone_number = message.contact.phone_number
     if await check_phone_number(phone_number):
         await state.update_data(phone_number=phone_number)
-        await message.answer(text=PHONE_NUMBER_EXISTS, reply_markup=await get_start_keyboard())
+        await message.answer(
+            text=PHONE_NUMBER_EXISTS,
+            reply_markup=await get_start_keyboard(),
+        )
     else:
         await message.answer(PHONE_NUMBER_NOT_EXISTS)
         await state.update_data(phone_number=None)
--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -1,11 +1,17 @@
 from database.db_core import UnitOfWork
 
-from config import AUTH_DB_USER, AUTH_DB_PASSWORD, AUTH_DB_DOMAIN, AUTH_DB_PORT, AUTH_DB_NAME
+from config import (
+    AUTH_DB_USER,
+    AUTH_DB_PASSWORD,
+    AUTH_DB_DOMAIN,
+    AUTH_DB_PORT,
+    AUTH_DB_NAME,
+)
 from database.db_core import PostgresConfig
 from database.user_dto import UserDto
 
 
-async def check_phone_number(phone_num) -> bool:
+async def check_phone_number(phone_num: str) -> bool:
     config = PostgresConfig(
         user_name=AUTH_DB_USER,
         password=AUTH_DB_PASSWORD,
@@ -17,10 +23,8 @@ async def check_phone_number(phone_num) -> bool:
     config.create_engine()
     uow = UnitOfWork(config)
     with uow.start() as session:
-        exists: bool = (
-            session.auth_repository.check_if_exists(
-                phone_num,
-            )
+        exists: bool = session.auth_repository.check_if_exists(
+            phone_num,
         )
 
     return exists

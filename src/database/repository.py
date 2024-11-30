67341c63a4f1bfb609382115c54ec207d77e0579
Ivan Svyatykh <zlativansvyat@mail.ru>
2024-11-30 19:27:39 +0500

    добавил метод для полкчения эвентов зв неделю

--- src/database/repository.py
+++ src/database/repository.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from typing import List
 from sqlalchemy import func
 from sqlalchemy.orm import Session
-
+import sqlalchemy
 from database.dto import CameraEventDto
 from database.is_db_models import MonitoringEvents
 
@@ -14,7 +14,7 @@ class AuthRepository:
     def __init__(self, session: Session):
         self.session = session
 
-    def check_if_exists(self, phone_number: str) -> bool:
+    async def check_if_exists(self, phone_number: str) -> bool:
         user: Users = (
             self.session.query(Users)
             .filter_by(phone_number=phone_number)
@@ -32,7 +32,7 @@ class MonitoringEventsRepository:
     def __init__(self, session: Session):
         self.session = session
 
-    def get_last_camera_event(
+    async def get_last_camera_event(
         self, camera_id: int, scenario_id: int
     ) -> CameraEventDto:
         latest_event: MonitoringEvents = (
@@ -45,7 +45,7 @@ class MonitoringEventsRepository:
             return None
         return CameraEventDto(latest_event)
 
-    def get_day_camera_event(
+    async def get_day_camera_event(
         self, camera_id: int, scenario_id: int, target_date: datetime
     ) -> List[CameraEventDto]:
         event_by_date: List[MonitoringEvents] = (
@@ -55,6 +55,33 @@ class MonitoringEventsRepository:
                 scenario_id=scenario_id,
             )
             .filter(func.date(MonitoringEvents.timestamp) == target_date)
+            .order_by(MonitoringEvents.timestamp)
+            .all()
+        )
+        if event_by_date is None:
+            return None
+
+        return [CameraEventDto(event) for event in event_by_date]
+
+    async def get_week_camera_event(
+        self,
+        camera_id: int,
+        scenario_id: int,
+        start_date: datetime,
+        end_date_date: datetime,
+    ) -> List[CameraEventDto]:
+        event_by_date: List[MonitoringEvents] = (
+            self.session.query(MonitoringEvents)
+            .filter_by(
+                camera_id=camera_id,
+                scenario_id=scenario_id,
+            )
+            .filter(
+                sqlalchemy.and_(
+                    MonitoringEvents.timestamp >= start_date,
+                    MonitoringEvents.timestamp <= end_date_date,
+                )
+            )
             .order_by(MonitoringEvents.timestamp)
             .all()
         )

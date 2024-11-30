e9911f9ed8764acf53c678e07a9439729cbb069d
Ivan Svyatykh <zlativansvyat@mail.ru>
2024-11-30 21:26:31 +0500

    добавил график людей за прошлую неедлю

--- src/services/kitchen_service.py
+++ src/services/kitchen_service.py
@@ -1,6 +1,5 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
-from datetime import timedelta
 import requests
 from database.dto import CameraEventDto
 from database.db_core import PostgresConfig, IsDBUnitOfWork
@@ -12,7 +11,10 @@ from config import (
     POSTGRES_PORT,
 )
 from typing import Tuple
-from services.utils.grapics_makers import create_hist_of_disribution
+from services.utils.grapics_makers import (
+    create_hist_of_day_disribution,
+    create_hist_of_week_disribution,
+)
 
 
 async def __get_photo_of_kitchen(
@@ -51,14 +53,33 @@ async def get_people_disribution_on_kitchen_by_day(
         y.append(len(event.boxes_cords["bboxes"]))
         x.append(event.timestamp + timedelta(hours=5))
 
-    path = Path(f"src/files/stat/{user_id}_stat.png")
+    path = Path(f"src/files/stat/{user_id}_stat_day.png")
 
-    await create_hist_of_disribution(y_data=y, x_data=x, path_to_png=path)
+    await create_hist_of_day_disribution(
+        y_data=y, x_data=x, path_to_png=path
+    )
     return path
 
 
+async def __get_start_end_last_week(
+    current_date: datetime,
+) -> Tuple[datetime, datetime]:
+
+    start = current_date.date() - timedelta(
+        days=(current_date.date().isoweekday()) + 6
+    )
+    end = current_date.date() - timedelta(
+        days=(current_date.date().isoweekday())
+    )
+
+    return (start, end)
+
+
 async def get_people_disribution_on_kitchen_by_week(
-    camera_id: int, scenario_id: int, date: datetime, user_id: int
+    camera_id: int,
+    scenario_id: int,
+    current_date: datetime,
+    user_id: int,
 ) -> Path:
     config = PostgresConfig(
         user_name=POSTGRES_USER,
@@ -70,9 +91,12 @@ async def get_people_disribution_on_kitchen_by_week(
 
     config.create_engine()
     uow = IsDBUnitOfWork(config)
+    start_last_week, end_last_week = await __get_start_end_last_week(
+        current_date
+    )
     with uow.start() as session:
-        events = await session.monitoring_events_repository.get_day_camera_event(
-            camera_id, scenario_id, date.date()
+        events = await session.monitoring_events_repository.get_week_camera_event(
+            camera_id, scenario_id, start_last_week, end_last_week
         )
     y = []
     x = []
@@ -80,9 +104,11 @@ async def get_people_disribution_on_kitchen_by_week(
         y.append(len(event.boxes_cords["bboxes"]))
         x.append(event.timestamp + timedelta(hours=5))
 
-    path = Path(f"src/files/kitchen_photo/stat_{user_id}.png")
+    path = Path(f"src/files/kitchen_photo/{user_id}_stat_week.png")
 
-    await create_hist_of_disribution(y_data=y, x_data=x, path_to_png=path)
+    await create_hist_of_week_disribution(
+        y_data=y, x_data=x, path_to_png=path
+    )
     return path
 
 

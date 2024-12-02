b8e7a48db333cba9b142597eaa497ff991b5e168
Ivan Svyatykh <zlativansvyat@mail.ru>
2024-12-02 19:55:47 +0500

    Доделал отправку статистики

--- src/handlers/statistic_handler.py
+++ src/handlers/statistic_handler.py
@@ -2,12 +2,11 @@ from datetime import datetime, timedelta
 from pathlib import Path
 from aiogram import Router
 from aiogram.types import CallbackQuery
-from aiogram.fsm.context import FSMContext
 from aiogram.types import FSInputFile
 from handlers.utils.answer import STATISTIC_MESS, NO_CAMERA_EVENTS
 from config import KITCHEN_ID, SCENARIO_ID
 from handlers.utils.keyboards import get_back_keyboard
-from services.kitchen_service import (
+from services.statistics_service import (
     get_people_disribution_on_kitchen_by_day,
 )
 
@@ -16,20 +15,23 @@ router = Router()
 
 @router.callback_query(lambda c: c.data == "statistics")
 async def kitchen_statistics(callback_query: CallbackQuery):
-    path: Path | None = await get_people_disribution_on_kitchen_by_day(
-        KITCHEN_ID,
-        SCENARIO_ID,
-        datetime.today() - timedelta(days=1),
-        callback_query.message.from_user.id,
-    )
-    if path is None:
-        await callback_query.message.answer(
-            text=NO_CAMERA_EVENTS, reply_markup=await get_back_keyboard()
+    for i in range(1, 4):
+        date = datetime.today() - timedelta(days=i)
+        path: Path | None = await get_people_disribution_on_kitchen_by_day(
+            KITCHEN_ID,
+            SCENARIO_ID,
+            date,
+            callback_query.message.from_user.id,
         )
-    else:
-        await callback_query.message.bot.send_photo(
-            photo=FSInputFile(path),
-            caption=STATISTIC_MESS,
-            chat_id=callback_query.message.chat.id,
-            reply_markup=await get_back_keyboard(),
-        )
+        reply_markup = await get_back_keyboard() if i == 3 else None
+        if path is None:
+            await callback_query.message.answer(
+                text=NO_CAMERA_EVENTS.format(date.date()),
+                reply_markup=reply_markup,
+            )
+        else:
+            await callback_query.message.answer_photo(
+                photo=FSInputFile(path),
+                caption=STATISTIC_MESS.format(date.date()),
+                reply_markup=reply_markup,
+            )
